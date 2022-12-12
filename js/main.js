495b3b8205b89e510e716545c32c1456887f5ad8
Samuel Affolder <swaff@uw.edu>
2022-12-12 14:26:07 -0800

    improving mobile version

--- js/main.js
+++ js/main.js
@@ -146,8 +146,18 @@
     }
   }
 
+  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+  function responsive_control() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
   // Initializing function called after the DOM is loaded in
- function init() {
+  function init() {
     map = new mapboxgl.Map({
       container: 'map', // container ID
       style: 'mapbox://styles/mapbox/light-v11',
@@ -160,6 +170,10 @@
       map.addImage('policeIcon', icon);
     });
 
+    let icon = document.getElementsByClassName("icon")[0];
+    icon.addEventListener('click', responsive_control);
+
     geojsonFetch();
+
   }
 })();
